2d8a76759006c20a475554153fa9c564c78f4d45
wangqimeng <wangqimeng@mininglamp.com>
2020-08-06 09:54:01 +0800

    add 102 二叉树的层序遍历

---
+++ leetcode/editor/cn/102/102_二叉树的层序遍历_test.go
@@ -0,0 +1,71 @@
+package leetcode_cn_102
+
+import (
+	. "go-leetcode-practice/leetcode/editor/cn/common"
+	"testing"
+)
+
+//给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+//
+//
+//
+// 示例：
+//二叉树：[3,9,20,null,null,15,7],
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+//
+//
+// 返回其层次遍历结果：
+//
+// [
+//  [3],
+//  [9,20],
+//  [15,7]
+//]
+//
+// Related Topics 树 广度优先搜索
+// 👍 585 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func levelOrder(root *TreeNode) [][]int {
+	// 返回结果
+	var ret [][]int
+	if root == nil {
+		return ret
+	}
+	q := []*TreeNode{root}
+	for i := 0; len(q) > 0; i++ {
+		ret = append(ret, []int{})
+		var p []*TreeNode
+		for j := 0; j < len(q); j++ {
+			node := q[j]
+			ret[i] = append(ret[i], node.Val)
+			if node.Left != nil {
+				p = append(p, node.Left)
+			}
+			if node.Right != nil {
+				p = append(p, node.Right)
+			}
+		}
+		q = p
+	}
+	return ret
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+func Test(t *testing.T) {
+
+}
